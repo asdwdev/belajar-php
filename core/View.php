eb8517b7ee353c55f2d0d64c7076e0d55327fbe6
asdwdev <asdwdev@gmail.com>
2025-08-31 20:11:16 +0700

    buat biar bisa nampilin data ala blade laravel

--- core/View.php
+++ core/View.php
@@ -4,16 +4,30 @@ class View
 {
     public static function render($view, $data = [])
     {
-        // extract array jadi variable
+        // ambil file view, misal "biodata.index" jadi "views/biodata/index.php"
+        $path = "views/" . str_replace('.', '/', $view) . ".php";
+
+        if (!file_exists($path)) {
+            throw new Exception("View $view tidak ditemukan");
+        }
+
+        // Extract data biar bisa dipakai langsung sebagai variabel
         extract($data);
 
-        // ubah titik jadi slash: "biodata.index" -> "views/biodata/index.php"
-        $viewFile = __DIR__ . '/../views/' . str_replace('.', '/', $view) . '.php';
+        // Ambil isi file view
+        $content = file_get_contents($path);
 
-        if (file_exists($viewFile)) {
-            include $viewFile;
-        } else {
-            echo "View $viewFile tidak ditemukan!";
-        }
+        // Parsing ala Blade: {{ $var }} -> <?= htmlspecialchars($var)
+        $content = preg_replace('/\{\{\s*(.+?)\s*\}\}/', '<?= htmlspecialchars($1) ?>', $content);
+
+        // Simpan ke file temporary
+        $tmp = tempnam(sys_get_temp_dir(), 'view_') . ".php";
+        file_put_contents($tmp, $content);
+
+        // Include file hasil parsing
+        include $tmp;
+
+        // Hapus file temp
+        unlink($tmp);
     }
 }
